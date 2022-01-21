1289d29e8f385d172d689fbd1e39502b9b5d5e85
opilane <opilane@HP-CZC6297BB1>
2022-01-21 10:28:32 +0200

    done

--- Grid.tunniplaan.py
+++ Grid.tunniplaan.py
@@ -11,7 +11,8 @@ def newwind(a):
 	a1=a.replace(" \n", "")
 	showinfo("tunni informatsioon",f"{a}")
 	newwd=Toplevel() #tk()
-	abc=Label(newwd,text=Lessons.get(a1))
+	abc=Label(newwd,text=Lessons.get(a1),font="Calibri 23",fg="black",justify=CENTER)
+	newwd.geometry("500x100")
 	abc.pack()
 
 wind=Tk()
